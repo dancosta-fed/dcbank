0d68cf8abadd58f1d5cca628947bde5b21a091f1
Daniel Costa <dancosta.id@gmail.com>
2022-04-15 16:47:40 -0300

    Couldn't make it work in production

--- src/components/services/api.ts
+++ src/components/services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3000/api', // development
-  baseURL: 'https://dcbank-oaf25szzy-dancosta-fed.vercel.app/', // production
+  // baseURL: 'https://dcbank-oaf25szzy-dancosta-fed.vercel.app/', // production
   
 })
--- src/index.tsx
+++ src/index.tsx
@@ -44,10 +44,10 @@ createServer({
   routes(){
     this.namespace = 'api'
 
-    this.get('api/transactions', () => {
+    this.get('/transactions', () => {
       return this.schema.all('transaction')
     })
-    this.post('api/transactions', (schema, request) => {
+    this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody)
 
       return schema.create('transaction', data)

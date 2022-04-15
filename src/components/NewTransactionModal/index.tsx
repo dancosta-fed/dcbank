b6a315bb09e37821074fa28905bc3f3d22c7f01b
Daniel Costa <dancosta.id@gmail.com>
2022-04-15 10:06:35 -0300

    modal is completely done with styles and btns. Just need to work

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { useState } from "react";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -11,6 +14,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionProps) {
+  const [type, setType] = useState("deposit");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -31,6 +36,34 @@ export function NewTransactionModal({
         <input placeholder="Title" />
 
         <input type="number" placeholder="Amount" />
+
+        <TransactionTypeContainer>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit")
+            }}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="income icon" />
+            <span>Income</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw")
+            }}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="outcome icon" />
+            <span>Outcome</span>
+          </RadioBox>
+
+        </TransactionTypeContainer>
 
         <input placeholder="Category" />
 

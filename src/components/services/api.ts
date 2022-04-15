2b556e4bc40d10e96d769957d2d6aeccc6ba0362
Daniel Costa <dancosta.id@gmail.com>
2022-04-15 15:17:44 -0300

    fixing baseURL for api

--- src/components/services/api.ts
+++ src/components/services/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api', // development
+  baseURL: 'https://dancosta-fed.github.io/dcbank/', // production
+  
   // headers: {} -> Um cabeçalho ou token de autenticação. Caso precise.
 })

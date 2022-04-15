642e6cdf0283a2632064eb285544949fb2074b8d
Daniel Costa <dancosta.id@gmail.com>
2022-04-15 16:39:05 -0300

    api isn't working on gh-pages. tried fixing

--- src/components/services/api.ts
+++ src/components/services/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3000/api', // development
-  baseURL: 'https://dancosta-fed.github.io/dcbank/', // production
+  baseURL: 'https://dcbank-oaf25szzy-dancosta-fed.vercel.app/', // production
   
-  // headers: {} -> Um cabeçalho ou token de autenticação. Caso precise.
 })
